258db9ebfc491820715df185103da24ed0a6cf32
Alistair Driscoll <pallythecoder@gmail.com>
2024-11-26 16:13:50 +0000

    Add: Checkout views and templates

--- checkout/forms.py
+++ checkout/forms.py
@@ -27,7 +27,7 @@ class OrderForm(forms.ModelForm):
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
-        for fields in self.fields:
+        for field in self.fields:
             if self.fields[field].required:
                 placeholder = f'{placeholders[field]} *'
             else:
